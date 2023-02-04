9277992da53bab0e0da1b79d925b5a89b7f96402
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-04 08:59:36 -0500

    update

--- one/character/main.js
+++ one/character/main.js
@@ -1,9 +1,10 @@
-const textarea = document.querySelector("textarea");
-const counter = document.getElementById("counter");
 
-textarea.addEventListener("input", function (e){
+const textarea = document.querySelector("textarea")
+const counter = document.getElementById("counter")
+
+textarea.addEventListener("input", function(e){
     const target = e.target;
     const max = target.getAttribute("maxLength")
     const current = target.value.length;
-    counter.innerHTML = `${current}/${max}`;
+    counter.innerHTML = `${current}/${max}`
 })
